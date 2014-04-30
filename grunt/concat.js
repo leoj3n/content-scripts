9ff66ce6e440ff9492a7c8bdbb60ca220391edb0
Joel Kuzmarski <leoj3n@gmail.com>
2014-04-30 07:53:31 -0500

    Update GitHub and YouTube

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,7 +3,7 @@ module.exports = {
     src: [
         '<%= config.app %>/sites/github/app/assets/css/dark.css',
         '<%= config.app %>/sites/github/dist/style.css',
-        '<%= config.app %>/sites/github/assets/css/fun.css'
+        '<%= config.app %>/sites/github/app/assets/css/fun.css'
     ],
     dest: '<%= config.app %>/sites/github/dist/style.css'
   }
