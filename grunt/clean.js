dc45aea9a4fa303c23bce28c5a23af68711ca6c5
Joel Kuzmarski <leoj3n@gmail.com>
2014-10-15 23:34:53 -0500

    StackOverflow grunt, dist, rename

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,11 +1,10 @@
 module.exports = {
-    chrome: {
-    },
-    github: {
+    all: {
         files: [{
             dot: true,
             src: [
                 '<%= config.app %>/sites/github/dist/style.css',
+                '<%= config.app %>/sites/stackoverflow/dist/style.css',
                 '<%= config.app %>/sites/youtube/dist/style.css'
             ]
         }]
--- grunt/concat.js
+++ grunt/concat.js
@@ -1,11 +1,16 @@
 module.exports = {
-  github: {
-    src: [
-        '<%= config.app %>/sites/github/app/GitHub-Dark/github-dark-github-dark-008080.build.min.css',
-        //'<%= config.app %>/sites/github/app/assets/css/dark.css',
-        '<%= config.app %>/sites/github/dist/style.css',
-        '<%= config.app %>/sites/github/app/assets/css/fun.css'
-    ],
-    dest: '<%= config.app %>/sites/github/dist/style.css'
-  }
+    github: {
+        src: [
+            '<%= config.app %>/sites/github/app/GitHub-Dark/github-dark-github-dark-008080.build.min.css',
+            '<%= config.app %>/sites/github/dist/style.css',
+            '<%= config.app %>/sites/github/app/assets/css/fun.css'
+        ],
+        dest: '<%= config.app %>/sites/github/dist/style.css'
+    },
+    stackoverflow: {
+        src: [
+            '<%= config.app %>/sites/stackoverflow/app/assets/css/structure.css'
+        ],
+        dest: '<%= config.app %>/sites/stackoverflow/dist/style.css'
+    }
 };
