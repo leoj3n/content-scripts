fc055766c46680f98469f3c532dc6469f77f1336
Joel Kuzmarski <leoj3n@gmail.com>
2014-10-17 03:56:24 -0500

    Move github-timeline submodule out of dist

--- grunt/run.js
+++ grunt/run.js
@@ -1,9 +1,15 @@
 module.exports = {
-    github: {
+    github_dark: {
         options: {
             cwd: process.cwd() + '/<%= config.app %>/sites/github/app/GitHub-Dark'
         },
         exec: 'npm install && cp ../build.json . && grunt minify'
+    },
+    github_timeline: {
+        options: {
+            cwd: process.cwd() + '/<%= config.app %>/sites/github/app/github-timeline'
+        },
+        exec: 'cp github-timeline.js ../../dist/github-timeline.js'
     }
 };
 
