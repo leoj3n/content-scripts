2d18d82c1bf9bebccf9d6d55520795fc7e7c0d2c
Joel Kuzmarski <leoj3n@gmail.com>
2015-01-26 10:26:00 -0600

    Improve scripts

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
+  console.log('previousVersion', details.previousVersion);
 });
 
 //
@@ -9,7 +9,9 @@ chrome.runtime.onInstalled.addListener(function (details) {
 //
 
 chrome.tabs.getSelected( null, function( tab ) {
+  if (!tab.url.includes('chrome://') && !tab.url.includes('chrome-devtools://')) {
     chrome.tabs.reload(tab.id);
+  }
 });
 
 //
@@ -17,19 +19,20 @@ chrome.tabs.getSelected( null, function( tab ) {
 //
 
 chrome.webRequest.onHeadersReceived.addListener(function(details) {
-    for (var i = 0; i < details.responseHeaders.length; i++) {
-        if (details.responseHeaders[i].name.toUpperCase() === 'CONTENT-SECURITY-POLICY') {
-            var begin = details.responseHeaders[i].value.split('img-src', 2);
-            var end = begin[1].split(';', 2);
-            details.responseHeaders[i].value = begin[0] + 'img-src \'self\' data: *;' + end[1];
-        }
+  for (var i = 0; i < details.responseHeaders.length; i++) {
+    if (details.responseHeaders[i].name.toUpperCase() === 'CONTENT-SECURITY-POLICY') {
+      var begin = details.responseHeaders[i].value.split('img-src', 2);
+      if (typeof begin[1] !== 'undefined') {
+        var end = begin[1].split(';', 2);
+        details.responseHeaders[i].value = begin[0] + 'img-src \'self\' data: *;' + end[1];
+      }
     }
-    return {
-        responseHeaders: details.responseHeaders
-    };
+  }
+  return {
+    responseHeaders: details.responseHeaders
+  };
 }, {
-    urls: [ '*://*.github.com/*' ],
-    types: [ 'main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'object', 'xmlhttprequest', 'other' ]
+  urls: [ '*://*.github.com/*' ],
+  types: [ 'main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'object', 'xmlhttprequest', 'other' ]
 }, [ 'blocking', 'responseHeaders' ]);
 
-console.log('background.js Event Page');
