db2c379e92a5d2472e8ae49e4322eeb82c5a3d59
Joel Kuzmarski <leoj3n@gmail.com>
2016-02-10 23:03:22 -0600

    Remove dead CSP code

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -67,27 +67,3 @@ chrome.tabs.getSelected( null, function( tab ) {
   }
 });
 
-//
-// Disable CSP on GitHub
-//
-
-/* jk: causing problems in 2016
-chrome.webRequest.onHeadersReceived.addListener(function(details) {
-  for (var i = 0; i < details.responseHeaders.length; i++) {
-    if (details.responseHeaders[i].name.toUpperCase() === 'CONTENT-SECURITY-POLICY') {
-      var begin = details.responseHeaders[i].value.split('img-src', 2);
-      if (typeof begin[1] !== 'undefined') {
-        var end = begin[1].split(';', 2);
-        details.responseHeaders[i].value = begin[0] + 'img-src \'self\' data: *;' + end[1];
-      }
-    }
-  }
-  return {
-    responseHeaders: details.responseHeaders
-  };
-}, {
-  urls: [ '*://*.github.com/*' ],
-  types: [ 'main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'object', 'xmlhttprequest', 'other' ]
-}, [ 'blocking', 'responseHeaders' ]);
-*/
-
