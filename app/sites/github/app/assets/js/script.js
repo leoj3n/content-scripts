0f152417922c14459107021cc0b1769a4ee2adf7
Joel Kuzmarski <leoj3n@gmail.com>
2016-07-30 05:34:56 -0500

    Avoid null/undefined errors in mutation observer

--- app/sites/github/app/assets/js/script.js
+++ app/sites/github/app/assets/js/script.js
@@ -2,20 +2,24 @@
 // Send Porter to bottom of page
 //
 
-var observer = new MutationObserver(function(mutations) {
-	mutations.forEach(function(mutation) {
-    var added = mutation.addedNodes[0];
-    if ( added.className === 'porter-plug' ) {
-      added.parentNode.appendChild(added);
-      observer.disconnect();
-    }
-	});
-});
+var container = document.getElementById('js-repo-pjax-container');
 
-var observerConfig = {
-	childList: true,
-  subtree: true
-};
+if (!! container) {
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      var added = mutation.addedNodes[0];
+      if ( !! added.className && added.className === 'porter-plug' ) {
+        added.parentNode.appendChild(added);
+        observer.disconnect();
+      }
+    });
+  });
 
-observer.observe(document.getElementById('js-repo-pjax-container'), observerConfig);
+  var observerConfig = {
+    childList: true,
+    subtree: true
+  };
+
+  observer.observe(container, observerConfig);
+}
 
